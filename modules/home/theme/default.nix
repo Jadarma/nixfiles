6f26a54f4742e8670ff24d63159e309cda90101a
Jadarma <dancristiancimpianu@gmail.com>
2024-01-14 22:45:37 +0200

    Use GTK theme.

--- modules/home/theme/default.nix
+++ modules/home/theme/default.nix
@@ -5,4 +5,12 @@
 
   # Material theme, best theme.
   colorScheme = nix-colors.colorSchemes.material;
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adapta-Nokto-Eta";
+      package = pkgs.adapta-gtk-theme;
+    };
+  };
 }
