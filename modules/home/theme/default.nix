d0589660474cfbc30ae4d77289450f4205126cb0
Jadarma <dancristiancimpianu@gmail.com>
2024-01-15 00:10:32 +0200

    Update icon theme and cursors.

--- modules/home/theme/default.nix
+++ modules/home/theme/default.nix
@@ -8,6 +8,17 @@
 
   gtk = {
     enable = true;
+
+    cursorTheme = {
+      name = "Catppuccin-Macchiato-Dark-Cursors";
+      package = pkgs.catppuccin-cursors.macchiatoDark;
+    };
+
+    iconTheme = {
+      name = "Papirus-Adapta-Nokto-Maia";
+      package = pkgs.papirus-maia-icon-theme;
+    };
+
     theme = {
       name = "Adapta-Nokto-Eta";
       package = pkgs.adapta-gtk-theme;
