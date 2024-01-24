6635633b604d020fa2074101db63ebcf6ad5ea9e
Jadarma <dancristiancimpianu@gmail.com>
2024-01-24 23:06:43 +0200

    Configure default wallpapers.

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -9,6 +9,27 @@
       variables = [ "--all" ];
     };
 
+    settings = {
+      exec-once = [ "hyprpaper" ];
+    };
+
     extraConfig = builtins.readFile ./hyprland.conf;
+  };
+
+  home.packages = with pkgs; [ hyprpaper ];
+
+  xdg.dataFile = {
+    "wallpapers/bg_center.png".source = ./wallpapers/bg_center.png;
+    "wallpapers/bg_left.png".source = ./wallpapers/bg_left.png;
+    "wallpapers/bg_right.png".source = ./wallpapers/bg_right.png;
+    "wallpapers/bg_full.png".source = ./wallpapers/bg_full.png;
+  };
+  xdg.configFile = {
+    "hypr/hyprpaper.conf".text = ''
+      ipc = off
+      splash = false
+      preload = ${config.xdg.dataHome}/wallpapers/bg_center.png
+      wallpaper = ,${config.xdg.dataHome}/wallpapers/bg_center.png
+    '';
   };
 }
