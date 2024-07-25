03c5ab916d9b3a03570be30cb4f3e4dcb91ed7b3
Jadarma <dancristiancimpianu@gmail.com>
2024-07-25 21:09:13 +0300

    Refactor HyprPaper to a Nix module.

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -2,6 +2,7 @@
 
   imports = [
     ./hyprlock.nix
+    ./hyprpaper.nix
   ];
 
   wayland.windowManager.hyprland = {
@@ -13,27 +14,6 @@
       variables = [ "--all" ];
     };
 
-    settings = {
-      exec-once = [ "hyprpaper" ];
-    };
-
     extraConfig = builtins.readFile ./hyprland.conf;
-  };
-
-  home.packages = with pkgs; [ hyprpaper ];
-
-  xdg.dataFile = {
-    "wallpapers/bg_center.png".source = ./wallpapers/bg_center.png;
-    "wallpapers/bg_left.png".source = ./wallpapers/bg_left.png;
-    "wallpapers/bg_right.png".source = ./wallpapers/bg_right.png;
-    "wallpapers/bg_full.png".source = ./wallpapers/bg_full.png;
-  };
-  xdg.configFile = {
-    "hypr/hyprpaper.conf".text = ''
-      ipc = off
-      splash = false
-      preload = ${config.xdg.dataHome}/wallpapers/bg_center.png
-      wallpaper = ,${config.xdg.dataHome}/wallpapers/bg_center.png
-    '';
   };
 }
---
+++ modules/home/hyprland/hyprpaper.nix
@@ -0,0 +1,29 @@
+{ pkgs, config, ... }: {
+
+  # Configure HyprPaper.
+  services.hyprpaper = {
+    enable = true;
+
+    settings = {
+      ipc = "off";
+      splash = false;
+      splash_offset = 2.0;
+
+      preload = [ "${config.xdg.dataHome}/wallpapers/bg_center.png" ];
+      wallpaper = [ ",${config.xdg.dataHome}/wallpapers/bg_center.png" ];
+    };
+  };
+
+  # IPC is disabled, so execute HyprPaper once at startup.
+  wayland.windowManager.hyprland.settings = {
+    exec-once = [ "hyprpaper" ];
+  };
+
+  # Import wallpaper image resources.
+  xdg.dataFile = {
+    "wallpapers/bg_center.png".source = ./wallpapers/bg_center.png;
+    "wallpapers/bg_left.png".source = ./wallpapers/bg_left.png;
+    "wallpapers/bg_right.png".source = ./wallpapers/bg_right.png;
+    "wallpapers/bg_full.png".source = ./wallpapers/bg_full.png;
+  };
+}
