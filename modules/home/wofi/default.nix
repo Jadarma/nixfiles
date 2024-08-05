d6aff362bcffacb4bed41d18534eeb7e43676892
Jadarma <dancristiancimpianu@gmail.com>
2024-08-05 20:01:44 +0300

    Move hyprland integration of wofi.

--- modules/home/wofi/default.nix
+++ modules/home/wofi/default.nix
@@ -16,4 +16,21 @@
     # TODO: Use color scheme variables for the colors.
     style = builtins.readFile ./style.css;
   };
+
+  # Hyprland integration.
+  wayland.windowManager.hyprland.settings = {
+    # NOTE: Bodgy hack here.
+    #       Because hyprland doesn't source the `home.sessionVariables`, force the menu to be launched by an intermediary
+    #       shell, that way ZSH will do the environment sourcing for you. This is important because without it, programs
+    #       won't have the same environment as your terminal, and IDEs won't be able to use GPG/SSH for instance.
+    bind = [
+      "SUPER, D, exec, /usr/bin/env zsh -ic 'wofi --show drun -D orientation=horizontal --lines=1 --prompt=\"Search Applications…\"'"
+    ];
+
+    # Use specific animation.
+    # TODO: Seems like only works for exit animation.
+    layerrule = [
+      "animation slide top, wofi"
+    ];
+  };
 }
