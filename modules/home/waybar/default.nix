b90ac9e1dea68fe97a36bb5a4fcd13d73f941f90
Jadarma <dancristiancimpianu@gmail.com>
2024-08-11 03:24:44 +0300

    Add mako status waybar widget.

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -9,9 +9,12 @@
     "waybar/config".source = ./config.json;
     "waybar/modules/clock.json".source = ./modules/clock.json;
     "waybar/modules/cpu.json".source = ./modules/cpu.json;
+    "waybar/modules/custom_mako.json".source = ./modules/custom_mako.json;
     "waybar/modules/idle_inhibitor.json".source = ./modules/idle_inhibitor.json;
     "waybar/modules/memory.json".source = ./modules/memory.json;
     "waybar/modules/pulseaudio.json".source = ./modules/pulseaudio.json;
+
+    "waybar/scripts/custom_mako.sh".source = ./scripts/custom_mako.sh;
   };
 
   # Hyprland integration.
