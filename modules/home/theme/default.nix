6460a6e7f2cf1c50e44cef4c4f55e089ac598daf
Jadarma <dancristiancimpianu@gmail.com>
2024-08-14 20:06:55 +0300

    Fix MaterialTheme for GTK4.
    Sadly Adapta theme only supports GTK2/3. Switched to Materia instead.

--- modules/home/theme/default.nix
+++ modules/home/theme/default.nix
@@ -4,7 +4,34 @@
   ];
 
   # Material theme, best theme.
-  colorScheme = nix-colors.colorSchemes.material;
+  # Imported from: https://github.com/tinted-theming/schemes/blob/spec-0.11/base16/material.yaml
+  # And slightly modified to suit my whims :)
+  colorScheme = {
+    name = "Material";
+    slug = "material";
+    author = "Nate Peterson";
+    variant = "dark";
+    palette = {
+      base00 = "263238";
+      base01 = "2e3c43";
+      base02 = "314549";
+      base03 = "546e7a";
+      base04 = "b2CCd6";
+      base05 = "eeffff";
+      base06 = "eeffff";
+      base07 = "eaeaea";
+      base08 = "f07178";
+      base09 = "f78C6C";
+      base0A = "ffCb6b";
+      base0B = "c3e88d";
+      base0C = "89ddff";
+      base0D = "82aaff";
+      base0E = "C792EA";
+      base0F = "FF5370";
+
+      accent = "16A085";
+    };
+  };
 
   # Configure the GTK Theme.
   # Note that while you could use `nix-colors` to autogenerate a Materia GTK theme, I found that the color scheme is a bit off.
@@ -24,10 +51,7 @@
       package = pkgs.papirus-maia-icon-theme;
     };
 
-    theme = {
-      name = "Adapta-Nokto-Eta";
-      package = pkgs.adapta-gtk-theme;
-    };
+    theme = (import ./materiaGtkTheme.nix { inherit pkgs; }) { scheme = config.colorScheme; };
   };
 
   # Configure QT to use Kvantum with the Adapta Dark theme, which will look close to the GTK theme.
