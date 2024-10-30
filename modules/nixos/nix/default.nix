00ead8b1a4313b2189b5547c28ef55fe9c5f8a44
Jadarma <dancristiancimpianu@gmail.com>
2024-10-30 16:15:41 +0200

    Make administrators trusted users.

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -3,6 +3,9 @@
   # Enable Nix Flakes.
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Make administrators trusted users.
+  nix.settings.trusted-users = [ "root" "@wheel" ];
+
   # Make unfree an opt-out instead of opt-in.
   nixpkgs.config.allowUnfree = lib.mkDefault true;
 
