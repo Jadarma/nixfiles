9b15416f1d95b4cf5a20c72e2588822a782dbf58
Jadarma <dancristiancimpianu@gmail.com>
2024-12-23 00:27:13 +0200

    Sync NixOS and HomeManager versions.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     };
 
     homeManager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
