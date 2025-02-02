6a63a1e8442a8197c617b135240205efdcdd7b15
Jadarma <dancristiancimpianu@gmail.com>
2025-02-03 00:20:55 +0200

    Refactor JadarmaLT to use new modules.

--- systems/x86_64-linux/JadarmaLT/home.nix
+++ systems/x86_64-linux/JadarmaLT/home.nix
@@ -2,28 +2,26 @@
 
   imports = [
     "${nixfiles}/modules/home/alacritty"
-    "${nixfiles}/modules/home/bat"
     "${nixfiles}/modules/home/cava"
-    "${nixfiles}/modules/home/direnv"
-    "${nixfiles}/modules/home/eza"
     "${nixfiles}/modules/home/firefox"
-    "${nixfiles}/modules/home/git"
-    "${nixfiles}/modules/home/gpg"
     "${nixfiles}/modules/home/htop"
     "${nixfiles}/modules/home/mako"
     "${nixfiles}/modules/home/neofetch"
     "${nixfiles}/modules/home/hyprland"
-    "${nixfiles}/modules/home/jetbrains"
     "${nixfiles}/modules/home/qalculate"
     "${nixfiles}/modules/home/scripts"
-    "${nixfiles}/modules/home/starship"
     "${nixfiles}/modules/home/theme"
     "${nixfiles}/modules/home/waybar"
     "${nixfiles}/modules/home/wofi"
-    "${nixfiles}/modules/home/xdg"
     "${nixfiles}/modules/home/zathura"
-    "${nixfiles}/modules/home/zsh"
+
+    "${nixfiles}/modules2/home/nixos"
   ];
+
+  nixfiles.home = {
+    cli.enable = true;
+    development.enable = true;
+  };
 
   home = {
     username = "dan";
@@ -33,6 +31,7 @@
     # Extra apps and packages.
     packages = with pkgs; [
       evince
+      jetbrains.idea-ultimate
       kdePackages.ark
       keepassxc
       pavucontrol
