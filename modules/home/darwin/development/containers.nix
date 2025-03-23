ae272324e59df96f78d98dbd150e662e18a2a815
Jadarma <dancristiancimpianu@gmail.com>
2025-03-23 20:13:58 +0200

    Add alias workaround for linking colima docker sock manually.

--- modules/home/darwin/development/containers.nix
+++ modules/home/darwin/development/containers.nix
@@ -11,5 +11,10 @@ let cfg = config.nixfiles.home.development.containers; in {
       colima
       docker-client
     ];
+
+    home.shellAliases = {
+      # Convenience to symlink colima as the default docker sock for programs that hardcode the path.
+      "colima-link" = ''sudo ln -s "$XDG_CONFIG_HOME/colima/default/docker.sock" /var/run/docker.sock'';
+    };
   };
 }
